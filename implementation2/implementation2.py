7d7f622a0dd8d4748f799fe498ec8d99ec32a7fc
harderg <harderg@oregonstate.edu>
2017-02-24 12:02:50 -0800

    finish assignment 2 and add writeup

--- implementation2/implementation2.py
+++ implementation2/implementation2.py
@@ -1,6 +1,7 @@
 import sys
 import random
 import numpy as np
+import timeit
 
 DIAG = 'G'
 LEFT = 'L'
@@ -65,34 +66,6 @@ def generate_random_inputs(n):
 
     return rand_seqs
 
-def unweighted_edit_dist(string_one, string_two):
-
-    m = len(string_one) + 1
-    n = len(string_two) + 1
-
-    dist_table = np.zeros((n, m), dtype=int)
-
-    for i in range(m):
-        dist_table[0][i] = i
-
-    for j in range(n):
-        dist_table[j][0] = j
-
-    for i in range(1, m):
-        for j in range(1, n):
-
-            if string_one[i - 1] == string_two[j - 1]:
-                diff = 0
-            else:
-                diff = 2
-
-            dist_table[j][i] = min( dist_table[j][i - 1] + 1,
-                                    dist_table[j - 1][i] + 1,
-                                    dist_table[j - 1][i - 1] + diff )
-
-    print dist_table
-    return dist_table[j][i]
-
 def unweighted_edit_dist_bt(string_one, string_two):
 
     m = len(string_one) + 1
@@ -151,41 +124,38 @@ def weighted_edit_dist_bt(string_one, string_two, cost_dict):
     #set the base column values for the distance table
     for i in range(m):
         dist_table[0][i] = cost_dict[DEL_CHAR][string_one[i - 1]]
+        #dist_table[0][i] = i
 
     #set the base row values for the distance table
     for j in range(n):
         dist_table[j][0] = cost_dict[DEL_CHAR][string_two[j - 1]]
+        #dist_table[j][0] = j
+
+    dist_table[0][0] = 0
 
     #iterate over the entire table
     for i in range(1, m):
         for j in range(1, n):
 
-            #if the characters are the same there is no cost, otherwise a substiution occurs
-            if string_one[i - 1] == string_two[j - 1]:
-                diff = 0
-            else:
-                diff = cost_dict[string_one[i - 1]][string_two[j - 1]]
-
             #get the minimum of our three options for alignment and store it
-            dist = min( dist_table[j][i - 1] + cost_dict[DEL_CHAR][string_one[i - 1]],
-                        dist_table[j - 1][i] + cost_dict[DEL_CHAR][string_two[j - 1]],
-                        dist_table[j - 1][i - 1] + diff )
+            dist_table[j][i] = min( dist_table[j][i - 1] + cost_dict[DEL_CHAR][string_one[i - 1]],
+                                    dist_table[j - 1][i] + cost_dict[DEL_CHAR][string_two[j - 1]],
+                                    dist_table[j - 1][i - 1] + cost_dict[string_one[i - 1]][string_two[j - 1]] )
 
-            #set the value of the distance table at our current place
-            dist_table[j][i] = dist
-
-            #build the back trace table
-            if dist == dist_table[j - 1][i - 1] + diff:     #ALIGN-move diag
+            #build the back trace table by determining what move we made
+            if dist_table[j][i] == dist_table[j - 1][i - 1] + cost_dict[string_one[i - 1]][string_two[j - 1]]:     #ALIGN-move diag
                 ptr[j][i] += DIAG
-            elif dist == dist_table[j][i - 1] + 1:          #DELETE-move down
+            elif dist_table[j][i] == dist_table[j][i - 1] + cost_dict[DEL_CHAR][string_one[i - 1]]:          #DELETE-move down
                 ptr[j][i] += DOWN
-            elif dist == dist_table[j - 1][i] + 1:          #INSERT-move left
+            elif dist_table[j][i] == dist_table[j - 1][i] + cost_dict[DEL_CHAR][string_two[j - 1]]:          #INSERT-move left
                 ptr[j][i] += LEFT
 
-    #print ptr
-    #print dist_table
+        #print dist_table
 
     new_str_one, new_str_two = backtrace(string_one, string_two, ptr)
+
+    #for pt in ptr:
+    #    print pt
 
     return dist_table[j][i], new_str_one, new_str_two
 
@@ -197,6 +167,7 @@ def backtrace(string_one, string_two, ptr):
 
     #perform the backtrace by starting in the top right corner and following the directions
     while k > 0 and l > 0:
+
         if ptr[l][k] == DIAG:
              new_str_one = string_one[k - 1] + new_str_one
              new_str_two = string_two[l - 1] + new_str_two
@@ -228,28 +199,33 @@ def backtrace(string_one, string_two, ptr):
 
     return new_str_one, new_str_two
 
+def run_and_time(n):
+
+    times = []
+    seqs = generate_random_inputs(n)
+    costs = build_cost_dict('imp2cost.txt')
+
+    for i in range(len(seqs)):
+
+        start = timeit.default_timer()
+        weighted_edit_dist_bt(seqs[i][0], seqs[i][1], costs)
+        end = timeit.default_timer()
+
+        times.append(end - start)
+
+    return sum(times) / len(times)
+
 def main():
 
     costs = build_cost_dict('imp2cost.txt')
+    seqs = read_input_file('imp2input.txt')
 
-    str1 = 'INTENTION'
-    str2 = 'EXECUTION'
+    f= open('imp2output.txt', 'w')
 
-    str3 = 'AAATGTGTGTGTTCCCCAACGATGTCTCTAGAAGACGAACATCCC'
-    str4 = 'ATGGAAACGTGAACCTAACTAACACATATGGATCCGACTGACGTTCTCTGATGTAGCCT'
+    for i in range(len(seqs)):
+        dist, newstr1, newstr2 = weighted_edit_dist_bt(seqs[i][0], seqs[i][1], costs)
 
-    str5 = 'ATCC'
-    str6 = 'TCAC'
-
-    str7 = 'AAATGTGTGTGTTCCCCAACGATGT'
-    str8 = 'ATGGAAACGTGAACCTAACTAACACA'
-
-    dist, newstr1, newstr2 = weighted_edit_dist_bt(str3, str4, costs)
-    #dist, newstr1, newstr2 = unweighted_edit_dist_bt(str3, str4)
-
-    print dist
-    print newstr1
-    print newstr2
+        f.write(newstr1 + ',' + newstr2 + ':' + str(dist) + '\n')
 
 if __name__ == '__main__':
     main()
